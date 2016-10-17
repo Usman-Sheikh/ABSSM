144cebf3dd7c5c24d36e43f573e323258a9ea18c
Usman Sheikh <sh.usmanikram@hotmail.com>
2016-10-18 01:35:32 +0500

    Error Fixed 2

--- ABSM/Global.asax.cs
+++ ABSM/Global.asax.cs
@@ -17,5 +17,11 @@ namespace ABSM
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
     }
 }
